58b3559c31a70ff302ead7e852a7d588b3fedf0a
Oluwajembola Orioke <116439666+OluwajembolaO@users.noreply.github.com>
2025-11-03 08:37:18 -0500

    11/3/25 SCREW PEDRO!!! #PedroPathingSucks - Jem 8:37 AM

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/RobotConstants.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.config;
+
+public class RobotConstants {
+    public static final String FRONT_LEFT  = "frontLeft";
+    public static final String FRONT_RIGHT = "frontRight";
+    public static final String BACK_LEFT   = "backLeft";
+    public static final String BACK_RIGHT  = "backRight";
+
+    public static final double WHEEL_DIAMETER_INCHES = 4.0;
+    public static final double TRACK_WIDTH_INCHES    = 15.0;
+
+    public static final double MAX_VEL   = 50.0;
+    public static final double MAX_ACCEL = 30.0;
+}
