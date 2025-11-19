48bf1f5f3894a852f7645b9e9980073a1c9c25cd
Oluwajembola Orioke <116439666+OluwajembolaO@users.noreply.github.com>
2025-11-18 23:56:28 -0500

    11/11/25 Added Pedro Pathing back ;-;. I pray everything goes well... - Jem 11:34 PM

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -16,14 +16,13 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(12) // Mass of robot (in Kilograms)
+            .mass(0) // Mass of robot (in Kilograms)
             //TODO
-            .forwardZeroPowerAcceleration(-22.05326607710397)
-            .lateralZeroPowerAcceleration(-298.0191190051714)
-            .translationalPIDFCoefficients(new PIDFCoefficients(0.065,0,0.0015,0.03)) //Its on PIDF on pp thing
-            .headingPIDFCoefficients(new PIDFCoefficients(.715, 0, 0.0025, 0.03 ))
-            .drivePIDFCoefficients(new FilteredPIDFCoefficients(.6,0,0.0001,0.6, 0.025)) //Also on thing
-            .centripetalScaling(0.005)
+            //.forwardZeroPowerAcceleration(0)
+            //.lateralZeroPowerAcceleration(0)
+            //.translationalPIDFCoefficients(new PIDFCoefficients(0,0,0,0)) //Its on PIDF on pp thing
+            //.drivePIDFCoefficients(new FilteredPIDFCoefficients(0,0,0,0)) //Also on thing
+            .centripetalScaling(0.000)
     ;
 
 
@@ -37,17 +36,18 @@ public class Constants {
             //TODO: Figure out which motors needs to be reversed 5:50 on the video - Jem
 
             .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
             //TODO A lot of testing - Jem
-            .xVelocity(81.11001562884476)
-            .yVelocity(90.06744504913571)
+            //.xVelocity(0)
+            //.yVelocity(0)
             ;
+
     public static PinpointConstants localizerConstants = new PinpointConstants()
             //TODO: I need to tune this - Jem
             .forwardPodY(-5)
-            .strafePodX(4.5)
+            .strafePodX(0.5)
 
             .distanceUnit(DistanceUnit.INCH) //Change units
             .hardwareMapName("pinpoint") //Change Name
@@ -55,14 +55,14 @@ public class Constants {
             .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
 
             //TODO Test encoders up +x left +y vice-versa -Jem
-            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED)
-            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED);
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
 
 
     public static PathConstraints pathConstraints = new PathConstraints(
             0.99,
             100,
-            1.4,
+            1,
             1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
@@ -71,6 +71,5 @@ public class Constants {
                 .pathConstraints(pathConstraints)
                 .mecanumDrivetrain(driveConstants)
                 .build();
-        //Hi
     }
 }
