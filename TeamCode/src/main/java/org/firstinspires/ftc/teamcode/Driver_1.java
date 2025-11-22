190fc30af644bef31561af0ac452c1c724c6d5d3
shock3371 <shocksword00@gmail.com>
2025-11-22 16:27:26 -0500

    11/11/2025 Updated Driver Controls, Created single and double driver files - Kenson 4:27 PM

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driver_1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driver_1.java
@@ -7,18 +7,18 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name="Driver1")
 public class Driver_1 extends OpMode {
-    //Represents the 4 motors I know we are probably using like 6-8 motors but this is just the basics
+    // the 4 drive motors and our shooting stuff - pls no more motor controls ;-;
     private DcMotor topLeftMotor;
     private DcMotor topRightMotor;
     private DcMotor rearLeftMotor;
     private DcMotor rearRightMotor;
     private DcMotor intake;
-    private DcMotor outtake1;   // Changed from single outtake
-    private DcMotor outtake2;  // Added second outtake motor
+    private DcMotor outtake1;   // first outtake motor
+    private DcMotor outtake2;   // second outtake motor
 
     @Override
     public void init() {
-        //Sets the motors
+        // grab all the motors from the config
         topLeftMotor = hardwareMap.get(DcMotor.class, "motor1");
         topRightMotor = hardwareMap.get(DcMotor.class, "motor2");
         rearLeftMotor = hardwareMap.get(DcMotor.class, "motor3");
@@ -27,59 +27,60 @@ public class Driver_1 extends OpMode {
         outtake1 = hardwareMap.get(DcMotor.class, "outtake1");
         outtake2 = hardwareMap.get(DcMotor.class, "outtake2");
 
-        //Why the reverse? Left side move opposite way from right so I would have to reverse ita
+        // right side motors naturally go backwards so we flip em
         topRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         rearRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        // Reverse one outtake motor so they spin opposite directions
+        // make one outtake spin the other way so they work together
         outtake1.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
     public void loop() {
-        // Intake control - me use left bumper
+        // left bumper = intake go
         intake.setPower(0);
         if (gamepad1.left_bumper){
             intake.setPower(1);
         }
 
-        // Outtake control - me use right bumper
-        // Both motors run at same power but opposite directions
+        // right bumper = outtake go brrr
+        // both motors spin at same power but opposite directions cause we reversed one
         outtake1.setPower(0);
         outtake2.setPower(0);
         if (gamepad1.right_bumper){
             outtake1.setPower(1);
-            outtake2.setPower(1);  // Will spin opposite due to setDirection
+            outtake2.setPower(1);  // spins opposite cause of setDirection above
         }
 
-        double speed = 0.65; // Default 50% power
-        double fwd = -gamepad1.left_stick_x; // forward/back - now using X axis idk dawg
-        double str = gamepad1.left_stick_y;  // left/right(Strafe) - now using Y axis
-        double rot = -gamepad1.right_stick_x; // rotation from right stick - using original working formula
+        double speed = 0.65; // normal driving speed
+        double fwd = -gamepad1.left_stick_x; // forward/back - using X axis idk dawg
+        double str = gamepad1.left_stick_y;  // side to side (strafe) - using Y axis
+        double rot = -gamepad1.right_stick_x; // spinning around - using the formula that actually works
 
-        float lt = gamepad1.left_trigger;   // Slow down trigger
-        float rt = gamepad1.right_trigger;  // Speed up trigger
+        float lt = gamepad1.left_trigger;   // slow mode trigger
+        float rt = gamepad1.right_trigger;  // fast mode trigger
 
-        // Adjust speed based on triggers (0.0 to 1.0)
+        // triggers make you go slower or faster
         if(lt > 0.5) {
-            speed = 0.4; // 25% power when left trigger pressed
+            speed = 0.4; // chill mode activated
         }
         if(rt > 0.5) {
-            speed = 0.9; // 75% power when right trigger pressed
+            speed = 0.9; // zoom zoom mode
         }
 
+        // mecanum drive math (seriously don't mess with this)
         double tLPower = fwd + str + rot;
         double rLPower = fwd - str - rot;
         double tRPower = fwd - str + rot;
         double rRPower = fwd + str - rot;
 
-        // Normalize the motor powers :> YIPEEEEE
+        // makes sure motors don't try to go over 100% :> YIPEEEEE
         double max = Math.max(1.0, Math.abs(tLPower));
         max = Math.max(max, Math.abs(rLPower));
         max = Math.max(max, Math.abs(tRPower));
         max = Math.max(max, Math.abs(rRPower));
 
-        // Apply normalized power multiplied by speed .-. (too much math imo)
+        // actually send power to the motors .-. (too much math imo)
         topLeftMotor.setPower((tLPower / max) * speed);
         topRightMotor.setPower((tRPower / max) * speed);
         rearLeftMotor.setPower((rLPower / max) * speed);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driver_2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driver_2.java
@@ -6,78 +6,140 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name="Driver2")
-public class
-Driver_2 extends OpMode {
-    //Represents the 4 motors I know we are probably using like 6-8 motors but this is just the basics
+public class Driver_2 extends OpMode {
+    // Drive motors for gamepad 1
     private DcMotor topLeftMotor;
     private DcMotor topRightMotor;
     private DcMotor rearLeftMotor;
     private DcMotor rearRightMotor;
 
+    // Intake/outtake motors for gamepad 2
+    private DcMotor intake;
+    private DcMotor outtake1;
+    private DcMotor outtake2;
+
+    // Outtake speed stuff - starts at 50% so we don't blow anything up lol
+    private double outtakeSpeed = 0.5;
+    private final double SPEED_INCREMENT = 0.1; // goes up/down by 10% each press
+    private final double MIN_SPEED = 0.3; // don't go below 30% or it's kinda useless
+    private final double MAX_SPEED = 1.0; // full send mode
+
+    // These stop you from spamming buttons and breaking everything
+    private boolean dpadUpPressed = false;
+    private boolean dpadDownPressed = false;
+
     @Override
     public void init() {
-        //Sets the motors
+        // grab all the motors from config
         topLeftMotor = hardwareMap.get(DcMotor.class, "motor1");
         topRightMotor = hardwareMap.get(DcMotor.class, "motor2");
         rearLeftMotor = hardwareMap.get(DcMotor.class, "motor3");
         rearRightMotor = hardwareMap.get(DcMotor.class, "motor4");
 
-        //Why the reverse? Left side move opposite way from right so I would have to reverse it
-        topLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        rearLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        outtake1 = hardwareMap.get(DcMotor.class, "outtake1");
+        outtake2 = hardwareMap.get(DcMotor.class, "outtake2");
+
+        // right side motors go backwards normally so we flip em
+        topRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        rearRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // make one outtake spin the other way so they work together
+        outtake1.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.addData("Outtake Speed", "%.0f%%", outtakeSpeed * 100);
+        telemetry.update();
     }
 
     @Override
     public void loop() {
-        double speed = 0.5; // Default 50% power
+        // ========== GAMEPAD 1: DRIVING STUFF ==========
+        double speed = 0.65; // normal speed
+        double fwd = -gamepad1.left_stick_x; // forward/back
+        double str = gamepad1.left_stick_y;  // side to side
+        double rot = -gamepad1.right_stick_x; // spinning
 
-        // D-pad controls instead of joysticks
-        double fwd = 0;
-        double str = 0;
-        double rot = gamepad1.right_stick_x; // Keep rotation on right stick
+        float lt = gamepad1.left_trigger;   // slow mode
+        float rt = gamepad1.right_trigger;  // zoom mode
 
-        // Forward/Backward with D-pad up/down
-        if(gamepad1.dpad_up) {
-            fwd = 1.0;
-        }
-        if(gamepad1.dpad_down) {
-            fwd = -1.0;
-        }
-
-        // Left/Right (strafe) with D-pad left/right
-        if(gamepad1.dpad_left) {
-            str = -1.0;
-        }
-        if(gamepad1.dpad_right) {
-            str = 1.0;
-        }
-
-        float lt = gamepad1.left_trigger;   // Slow down trigger
-        float rt = gamepad1.right_trigger;  // Speed up trigger
-
-        // Adjust speed based on triggers (0.0 to 1.0)
+        // triggers change how fast we go
         if(lt > 0.5) {
-            speed = 0.25; // 25% power when left trigger pressed
+            speed = 0.4; // chill mode
         }
         if(rt > 0.5) {
-            speed = 0.75; // 75% power when right trigger pressed
+            speed = 0.9; // gotta go fast
         }
 
+        // mecanum drive math (don't touch this unless you know what you're doing)
         double tLPower = fwd + str + rot;
-        double rLPower = fwd - str + rot;
-        double tRPower = fwd - str - rot;
+        double rLPower = fwd - str - rot;
+        double tRPower = fwd - str + rot;
         double rRPower = fwd + str - rot;
 
-        // Normalize the motor powers
+        // makes sure motors don't try to go over 100%
         double max = Math.max(1.0, Math.abs(tLPower));
         max = Math.max(max, Math.abs(rLPower));
         max = Math.max(max, Math.abs(tRPower));
         max = Math.max(max, Math.abs(rRPower));
 
-        // Apply normalized power multiplied by speed
+        // actually make the motors go
         topLeftMotor.setPower((tLPower / max) * speed);
         topRightMotor.setPower((tRPower / max) * speed);
         rearLeftMotor.setPower((rLPower / max) * speed);
         rearRightMotor.setPower((rRPower / max) * speed);
+
+        // ========== GAMEPAD 2: INTAKE/OUTTAKE CONTROLS ==========
+
+        // left bumper = intake go brrr
+        if (gamepad2.left_bumper) {
+            intake.setPower(1.0);
+        } else if (gamepad2.left_trigger > 0.5) {
+            // left trigger = oops wrong way
+            intake.setPower(-1.0);
+        } else {
+            intake.setPower(0);
+        }
+
+        // dpad up/down = make outtake faster/slower (just in case you guys are slow)
+        if (gamepad2.dpad_up && !dpadUpPressed) {
+            outtakeSpeed += SPEED_INCREMENT;
+            if (outtakeSpeed > MAX_SPEED) {
+                outtakeSpeed = MAX_SPEED; // can't go above 100%
+            }
+            dpadUpPressed = true;
+        } else if (!gamepad2.dpad_up) {
+            dpadUpPressed = false;
+        }
+
+        if (gamepad2.dpad_down && !dpadDownPressed) {
+            outtakeSpeed -= SPEED_INCREMENT;
+            if (outtakeSpeed < MIN_SPEED) {
+                outtakeSpeed = MIN_SPEED; // don't go too slow
+            }
+            dpadDownPressed = true;
+        } else if (!gamepad2.dpad_down) {
+            dpadDownPressed = false;
+        }
+
+        // right bumper = outtake go brrr at whatever speed we set
+        if (gamepad2.right_bumper) {
+            outtake1.setPower(outtakeSpeed);
+            outtake2.setPower(outtakeSpeed);
+        } else if (gamepad2.right_trigger > 0.5) {
+            // right trigger = reverse outtake if you messed up
+            outtake1.setPower(-outtakeSpeed);
+            outtake2.setPower(-outtakeSpeed);
+        } else {
+            outtake1.setPower(0);
+            outtake2.setPower(0);
+        }
+
+        // shows stuff on the driver station so you know what's happening
+        telemetry.addData("Drive Speed", "%.0f%%", speed * 100);
+        telemetry.addData("Outtake Speed", "%.0f%%", outtakeSpeed * 100);
+        telemetry.addData("Intake Active", gamepad2.left_bumper);
+        telemetry.addData("Outtake Active", gamepad2.right_bumper);
+        telemetry.update();
     }
 }
